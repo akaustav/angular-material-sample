3810dbc79bd71767b09e7e00cc900b74ecdfcfde
Ameet Kaustav <akaustav@users.noreply.github.com>
2025-11-10 04:31:03 -0700

    Add content to HeaderComponent
    - Angular Material toolbar
    - Theme selection

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { MatToolbar } from '@angular/material/toolbar';
+
+export interface AppTheme {
+  name: string;
+  icon: string;
+}
+
+const themes: AppTheme[] = [
+  { name: 'system', icon: 'desktop_windows' },
+  { name: 'light', icon: 'light_mode' },
+  { name: 'dark', icon: 'dark_mode' },
+];
+
+export type ThemeType = (typeof themes)[number]['name'];
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    MatIcon,
+    MatIconButton,
+    MatMenu,
+    MatMenuItem,
+    MatMenuTrigger,
+    MatToolbar,
+    TitleCasePipe,
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  private appTheme = signal<ThemeType>('system');
 
+  public selectedTheme = computed(
+    (): AppTheme =>
+      themes.find((t: AppTheme): boolean => t.name === this.appTheme()) ??
+      themes[0]
+  );
+
+  public getThemes(): AppTheme[] {
+    return themes;
+  }
+
+  public setTheme(theme: ThemeType): void {
+    this.appTheme.set(theme);
+  }
 }
