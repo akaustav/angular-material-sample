c1f41de541473317a627fc664c41789b63ec0011
Ameet Kaustav <akaustav@users.noreply.github.com>
2025-11-11 23:42:53 -0700

    Lazy load HomeComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/home/home.component').then((m) => m.HomeComponent),
+  },
 ];
