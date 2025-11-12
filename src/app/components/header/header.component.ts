ec11d78102e8870bc0ce94a2ad7f87c5cedcb5b8
Ameet Kaustav <akaustav@users.noreply.github.com>
2025-11-11 23:48:55 -0700

    Add Dark Mode

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,22 +1,10 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TitleCasePipe } from '@angular/common';
 import { MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 import { MatToolbar } from '@angular/material/toolbar';
-
-export interface AppTheme {
-  name: string;
-  icon: string;
-}
-
-const themes: AppTheme[] = [
-  { name: 'system', icon: 'desktop_windows' },
-  { name: 'light', icon: 'light_mode' },
-  { name: 'dark', icon: 'dark_mode' },
-];
-
-export type ThemeType = (typeof themes)[number]['name'];
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-header',
@@ -33,19 +21,5 @@ export type ThemeType = (typeof themes)[number]['name'];
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  private appTheme = signal<ThemeType>('system');
-
-  public selectedTheme = computed(
-    (): AppTheme =>
-      themes.find((t: AppTheme): boolean => t.name === this.appTheme()) ??
-      themes[0]
-  );
-
-  public getThemes(): AppTheme[] {
-    return themes;
-  }
-
-  public setTheme(theme: ThemeType): void {
-    this.appTheme.set(theme);
-  }
+  protected themeService = inject(ThemeService);
 }
--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { computed, effect, inject, Injectable, Signal, signal } from '@angular/core';
+
+export interface AppTheme {
+  name: string;
+  icon: string;
+}
+
+const themes: AppTheme[] = [
+  { name: 'system', icon: 'desktop_windows' },
+  { name: 'light', icon: 'light_mode' },
+  { name: 'dark', icon: 'dark_mode' },
+];
+
+export type ThemeType = (typeof themes)[number]['name'];
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ThemeService {
+  private document = inject(DOCUMENT);
+  private appTheme = signal<ThemeType>('system');
 
-  constructor() { }
+  constructor() {
+    effect((): void => {
+      const appTheme = this.appTheme();
+      const colorScheme = appTheme === 'system' ? 'light dark' : appTheme;
+      this.document.body.style.setProperty('color-scheme', colorScheme);
+    });
+  }
+
+  public selectedTheme: Signal<AppTheme> = computed(
+    (): AppTheme =>
+      themes.find((t: AppTheme): boolean => t.name === this.appTheme()) ??
+      themes[0]
+  );
+
+  public getThemes(): AppTheme[] {
+    return themes;
+  }
+
+  public setTheme(theme: ThemeType): void {
+    this.appTheme.set(theme);
+  }
 }
